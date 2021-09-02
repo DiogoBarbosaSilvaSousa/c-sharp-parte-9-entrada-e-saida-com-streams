7b4229a6968adf53d21bcff9e1521acf5a0f1330
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-09-02 09:37:16 -0300

    Lendo arquivos de texto

--- ByteBank/ByteBankImportacaoExportacao/Program.cs
+++ ByteBank/ByteBankImportacaoExportacao/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ByteBankImportacaoExportacao.Modelos;
 using System;
 using System.Collections.Generic;
+using System.IO; // IO = Input e Output
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,38 @@ namespace ByteBankImportacaoExportacao
     { 
         static void Main(string[] args) 
         {
+
+            var enderecoDoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024]; // 1 kb
+            var numeroDeBytesLidos = -1;
+
+            while(numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+           
+
             Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            //var utf8 = new UTF8Encoding();
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer);
+
+            Console.WriteLine(texto);
+
+            /*foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }*/
         }
     }
 } 
